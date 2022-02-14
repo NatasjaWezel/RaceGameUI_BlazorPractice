d590fe17db2eb52906c3b5d9e52c92206d6c42bc
Natasja Wezel <natasjawezel@hotmail.com>
2022-02-14 16:51:54 +0100

    name can not be null.

--- RaceGameUI_BlazorPractice.Model/BettorEntityModel.cs
+++ RaceGameUI_BlazorPractice.Model/BettorEntityModel.cs
@@ -3,7 +3,7 @@ namespace RaceGameUI_BlazorPractice.Model
 {
     public class BettorEntityModel
     {
-        public String Name { get; set; }
+        public String Name { get; set; } = "";
         public int StartingCash { get; set; }
         public int CurrentCash { get; set; }
     }
